10ab09f9b4e4b40ad504df788192511e78266a67
s.myakishev <s.myakishev@estp.ru>
2016-05-16 13:17:29 +0300

    fix error

--- rgit.sh
+++ rgit.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
+dir=$(pwd)
 repos=$(find . -type d -name .git | xargs -n 1 dirname)
 
 for repo in $repos;
 do
     echo;
     echo $repo;
+    cd "$dir/$repo"
     git $*;
 done
